9b13ff5874a12bcff5edfed9371381343a2a80a8
0fakhri <indsmnz15@gmail.com>
2021-12-13 08:42:01 +0700

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-66kr**==2n@plzz@ay^uzfzs#zwlhlzmdg4bci3xt-z7woou2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
